38d994d459a5de264ef5d11ef3811d3e53666f72
Pratik Bhosale <pratikbhosale0003@gmail.com>
2024-01-25 23:41:47 +0530

    view part done

--- app.js
+++ app.js
@@ -2,10 +2,30 @@ const BASE_URL ="https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/c
 
 const dropdown = document.querySelectorAll(".dropdown select");
 for(let select of dropdown){
-for(currCode in countryList) {
+for( currCode in countryList) {
     // console.log(currCode, countryList[currCode]);
     let newOption = document.createElement("option");
     newOption.innerText = currCode ;
+    newOption.value= currCode;
+    if(select.name ==="from"&& currCode ==="USD"){
+        newOption.selected ="Selected";
+    }else if(select.name ==="to"&& currCode ==="INR"){
+        newOption.selected ="Selected";
+    }
     select.append(newOption);
-} 
+}
+
+select.addEventListener("change", (evt )=>{
+updateFlag(evt.target);
+})
+}
+
+const updateFlag = (element) =>{
+    // console.log(element.value);
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+
 }
